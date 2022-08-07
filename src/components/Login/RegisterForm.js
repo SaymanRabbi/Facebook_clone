c429968187f2f992c93f9d4fb14bd199ac288d4a
sayman <mdrabbi.asm@gmail.com>
2022-08-07 10:52:31 +0600

    register form

--- src/components/Login/RegisterForm.js
+++ src/components/Login/RegisterForm.js
@@ -1,132 +1,148 @@
 import { Form, Formik } from "formik";
 import { useState } from "react";
-import Registerinput from "../inputs/Registerinput/Registerinput";
+import RegisterInput from "../inputs/Registerinput/Registerinput";
+
 const userInfos = {
   first_name: "",
   last_name: "",
   email: "",
   password: "",
-  bYear: "",
-  bMonth: "",
-  bDay: "",
+  bYear: new Date().getFullYear(),
+  bMonth: new Date().getMonth()+1,
+  bDay: new Date().getDate(),
   gender: "",
 };
+export default function RegisterForm() {
+  const yearTemp = new Date().getFullYear()
+  const [user, setUser] = useState(userInfos);
+  const { first_name, last_name, email, password, bYear, bDay, bMonth, gender } = user;
+  //get years
+  const years = Array.from(new Array(108), (val, index) => yearTemp - index);
+  //get months
+  const months = Array.from(new Array(12), (val, index) => index + 1);
+  //get days
+  const getDays = () => {
+    return new Date(bYear,bMonth,0).getDate();
+  };
+  const days = Array.from(new Array(getDays()), (val, index) => index + 1);
 
-const RegisterForm = () => {
-    const [user, setUser] = useState(userInfos);
-    const handleRegisterChange = (e) => {
-      const { name, value } = e.target;
-      setUser({ ...user, [name]: value });
-    };
-    return (
-      <div className="blur">
-        <div className="register">
-          <div className="register_header">
-            <i className="exit_icon"></i>
-            <span>Sign Up</span>
-            <span>it's quick and easy</span>
-          </div>
-          <Formik>
-            {(formik) => (
-              <Form className="register_form">
-                <div className="reg_line">
-                  <Registerinput
-                    type="text"
-                    placeholder="First name"
-                    name="first_name"
-                    onChange={handleRegisterChange}
-                  />
-                  <Registerinput
-                    type="text"
-                    placeholder="Surname"
-                    name="last_name"
-                    onChange={handleRegisterChange}
-                  />
-                </div>
-                <div className="reg_line">
-                  <Registerinput
-                    type="text"
-                    placeholder="Mobile number or email address"
-                    name="email"
-                    onChange={handleRegisterChange}
-                  />
-                </div>
-                <div className="reg_line">
-                  <Registerinput
-                    type="password"
-                    placeholder="New password"
-                    name="password"
-                    onChange={handleRegisterChange}
-                  />
-                </div>
-                <div className="reg_col">
-                  <div className="reg_line_header">
-                    Date of birth <i className="info_icon"></i>
-                  </div>
-                  <div className="reg_grid">
-                    <select name="bDay">
-                      <option>15</option>
-                    </select>
-                    <select name="bMonth">
-                      <option>15</option>
-                    </select>
-                    <select name="bYear">
-                      <option>15</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="reg_col">
-                  <div className="reg_line_header">
-                    Gender <i className="info_icon"></i>
-                  </div>
-                  <div className="reg_grid">
-                    <label htmlFor="male">
-                      Male
-                      <input
-                        type="radio"
-                        name="gender"
-                        id="male"
-                        value="male"
-                        onChange={handleRegisterChange}
-                      />
-                    </label>
-                    <label htmlFor="female">
-                      Female
-                      <input
-                        type="radio"
-                        name="gender"
-                        id="female"
-                        value="female"
-                        onChange={handleRegisterChange}
-                      />
-                    </label>
-                    <label htmlFor="custom">
-                      Custom
-                      <input
-                        type="radio"
-                        name="gender"
-                        id="custom"
-                        value="custom"
-                        onChange={handleRegisterChange}
-                      />
-                    </label>
-                  </div>
-                </div>
-                <div className="reg_infos">
-                  By clicking Sign Up, you agree to our{" "}
-                  <span>Terms, Data Policy &nbsp;</span>
-                  and <span>Cookie Policy.</span> You may receive SMS
-                  notifications from us and can opt out at any time.
-                </div>
-                <div className="reg_btn_wrapper">
-                  <button className="blue_btn open_signup">Sign Up</button>
-                </div>
-              </Form>
-            )}
-          </Formik>
+  const handleRegisterChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  return (
+    <div className="blur">
+      <div className="register">
+        <div className="register_header">
+          <i className="exit_icon"></i>
+          <span>Sign Up</span>
+          <span>it's quick and easy</span>
         </div>
+        <Formik>
+          {(formik) => (
+            <Form className="register_form">
+              <div className="reg_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="First name"
+                  name="first_name"
+                  onChange={handleRegisterChange}
+                />
+                <RegisterInput
+                  type="text"
+                  placeholder="Surname"
+                  name="last_name"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="Mobile number or email address"
+                  name="email"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_line">
+                <RegisterInput
+                  type="password"
+                  placeholder="New password"
+                  name="password"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Date of birth <i className="info_icon"></i>
+                </div>
+                <div className="reg_grid">
+                  <select name="bDay" value={bDay} onChange={handleRegisterChange}>
+                    {
+                      days.map((day, i) => <option key={i}>{day}</option>)
+                   }
+                  </select>
+                  <select name="bMonth" value={bMonth} onChange={handleRegisterChange}>
+                    {
+                      months.map((month, index) => <option key={index}>{month}</option> )
+                   }
+                  </select>
+                  <select name="bYear" value={bYear} onChange={handleRegisterChange}>
+                    {
+                      years?.map((year, index) => <option key={index}>{year}</option>)
+                    }
+                  </select>
+                </div>
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Gender <i className="info_icon"></i>
+                </div>
+                <div className="reg_grid">
+                  <label htmlFor="male">
+                    Male
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="male"
+                      value="male"
+                      onChange={handleRegisterChange}
+                    />
+                  </label>
+                  <label htmlFor="female">
+                    Female
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="female"
+                      value="female"
+                      onChange={handleRegisterChange}
+                    />
+                  </label>
+                  <label htmlFor="custom">
+                    Custom
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="custom"
+                      value="custom"
+                      onChange={handleRegisterChange}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className="reg_infos">
+                By clicking Sign Up, you agree to our{" "}
+                <span>Terms, Data Policy &nbsp;</span>
+                and <span>Cookie Policy.</span> You may receive SMS
+                notifications from us and can opt out at any time.
+              </div>
+              <div className="reg_btn_wrapper">
+                <button className="blue_btn open_signup">Sign Up</button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
-    );
-};
-
-export default RegisterForm;
+    </div>
+  );
+}
