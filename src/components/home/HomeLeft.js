4a06950982e234267090fab362274d012568acf1
sayman <mdrabbi.asm@gmail.com>
2022-08-12 21:04:15 +0600

    home left link

---
+++ src/components/home/HomeLeft.js
@@ -0,0 +1,27 @@
+import { left } from "../../data/home";
+import { Link } from "react-router-dom";
+import LeftLink from "./LeftLink";
+import './HomeLeft.css'
+
+const HomeLeft = ({user}) => {
+    return (
+        <div className="left_home">
+        <Link to="/profile" className="left_link hover1">
+          <img src={user?.picture} alt="" />
+          <span>
+            {user?.first_name} {user.last_name}
+          </span>
+        </Link>
+        {left.slice(0, 8).map((link, i) => (
+          <LeftLink
+            key={i}
+            img={link.img}
+            text={link.text}
+            notification={link.notification}
+          />
+        ))}
+      </div>
+    );
+};
+
+export default HomeLeft;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,11 @@
+import { useSelector } from "react-redux";
 import Header from "../../components/Header/Header";
+import HomeLeft from "../../components/home/HomeLeft";
 export default function Home() {
-  
+  //user
+  const { user } = useSelector((user) => ({ ...user }))
   return <div>
 <Header/>
-
+    <HomeLeft user={user} />
 </div>;
 }
