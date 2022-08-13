e8a3d6c54c9c1912365d315b64682748acf6597a
sayman <mdrabbi.asm@gmail.com>
2022-08-13 13:32:00 +0600

    responsive home story

--- src/components/home/Stroy/Stroies.js
+++ src/components/home/Stroy/Stroies.js
@@ -2,7 +2,11 @@ import './Stroy.css'
 import { ArrowRight, Plus } from "../../../svg";
 import { stories } from "../../../data/home";
 import Stroy from './Stroy';
+import {useMediaQuery} from 'react-responsive'
 const Stroies = () => {
+  const query1175px = useMediaQuery({query: "(max-width: 1175px)"});
+  const query1030px = useMediaQuery({query: "(max-width: 1030px)"});
+  const max =query1030px?5: query1175px? 4: stories.length;
     return (
         <div className="stories">
         <div className="create_story_card">
@@ -16,7 +20,7 @@ const Stroies = () => {
           </div>
           <div className="story_create_text">Create Story</div>
         </div>
-        {stories.map((story, i) => (
+        {stories.slice(0,max).map((story, i) => (
           <Stroy  story={story} />
         ))}
         <div className="white_circle">
