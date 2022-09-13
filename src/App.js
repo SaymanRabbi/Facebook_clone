7bb0e5f5f26f86a7dc8f865ab4e2b88e2cd723d1
saymanrabbi <mdrabbi.asm@gmail.com>
2022-09-13 22:18:36 +0600

    createPostPopup

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import LoginRoutes from "./Routes/LoginRoutes";
 import NotLoginRoutes from "./Routes/NotLoginRoutes";
 import Activate from "./pages/home/activate";
 import Reset from "./pages/Reset/Reset";
+import CreatePostPopup from "./components/CreatePostPopup/CreatePostPopup";
+import { useSelector } from "react-redux";
 function App() {
+  const { user } = useSelector((state) => ({ ...state }));
   return (
     <div>
+      <CreatePostPopup user={user} />
       <Routes>
-        
-        <Route element={<LoginRoutes/>}>
-        <Route path="/profile" element={<Profile />} exact />
-        <Route path="/" element={<Home />} exact />
-        <Route path="/activate/:token" element={<Activate/>}/>
+        <Route element={<LoginRoutes />}>
+          <Route path="/profile" element={<Profile />} exact />
+          <Route path="/" element={<Home />} exact />
+          <Route path="/activate/:token" element={<Activate />} />
         </Route>
-        <Route element={<NotLoginRoutes/>}>
-        <Route path="/login" element={<Login />} exact />
+        <Route element={<NotLoginRoutes />}>
+          <Route path="/login" element={<Login />} exact />
         </Route>
-        <Route path="/reset" element={<Reset/>}></Route>
+        <Route path="/reset" element={<Reset />}></Route>
       </Routes>
     </div>
   );
