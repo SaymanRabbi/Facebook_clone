238b24796b411810406d2cbbb31ed624ee7a9d09
saymanrabbi <mdrabbi.asm@gmail.com>
2022-09-13 22:59:59 +0600

    emoji picker

--- src/components/CreatePostPopup/CreatePostPopup.js
+++ src/components/CreatePostPopup/CreatePostPopup.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./CreatePostPopup.css";
+import Picker from "emoji-picker-react";
 
 const CreatePostPopup = ({ user }) => {
   const [text, setText] = useState("");
   const [showPrev, setShowPrev] = useState(false);
+  const [picker, setPicker] = useState(false);
+  const textRef = useRef(null);
+  const [cursorPosition, setCursorPosition] = useState();
+  useEffect(() => {
+    textRef.current.selectionEnd = cursorPosition;
+  }, [cursorPosition]);
+  const handleEmoji = (e, { emoji }) => {
+    const ref = textRef.current;
+    ref.focus();
+    const start = text.substring(0, ref.selectionStart);
+    const end = text.substring(ref.selectionStart);
+    const newText = start + emoji + end;
+    setText(newText);
+    setCursorPosition(start.length + emoji.length);
+  };
   return (
     <div className="blur">
       <div className="postBox">
@@ -30,7 +46,8 @@ const CreatePostPopup = ({ user }) => {
         {!showPrev && (
           <div className="flex_center">
             <textarea
-              maxlength="100"
+              ref={textRef}
+              maxLength="100"
               value={text}
               placeholder={`What's on your mind, ${user.first_name}`}
               className="post_input"
@@ -39,7 +56,18 @@ const CreatePostPopup = ({ user }) => {
           </div>
         )}
         <div className="post_emojis_wrap">
-          <div className="comment_emoji_picker rlmove"></div>
+          {picker && (
+            <div className="comment_emoji_picker rlmove">
+              <Picker onEmojiClick={handleEmoji} />
+            </div>
+          )}
+          <img src="../../../icons/colorful.png" alt="" />
+          <i
+            className="emoji_icon_large"
+            onClick={() => {
+              setPicker((prev) => !prev);
+            }}
+          ></i>
         </div>
       </div>
     </div>
