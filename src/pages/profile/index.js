98847920fa78ac4b4eb2dad2ff5fffc2f5a65090
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-20 19:15:58 +0600

    select cover photo

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import Header from "../../components/Header/Header";
@@ -7,6 +7,8 @@ import { profilereducer } from "../../func/reducers";
 import './style.css';
 
 export default function Profile() {
+  // covermenu
+  const [showCoverMenu, setShowCoverMenu] = useState(true);
   const navigate = useNavigate()
   const { user } = useSelector((state) => ({ ...state }));
   const {username} = useParams();
@@ -50,6 +52,21 @@ export default function Profile() {
             profile.cover &&
             <img src={profile.cover} alt="cover" className="cover" />
           }
+          <div className="update_cover_wrapper">
+            <div className="open_cover_update" onClick={()=>setShowCoverMenu(!showCoverMenu)}>
+              <i className="camera_filled_icon"></i>
+              Add Cover Photo
+            </div>
+            {
+              showCoverMenu && <div className="open_cover_menu">
+                <div className="open_cover_menu_item">
+                  <i className="photo_icon">
+                  </i>
+                  select photo
+                </div>
+              </div>
+            }
+          </div>
         </div>
       </div>
     </div>
