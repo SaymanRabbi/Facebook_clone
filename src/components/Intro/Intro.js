ee30f389ef968946df3bb2b63dc742badc37425a
saymanrabbi <mdrabbi.asm@gmail.com>
2022-12-03 18:37:50 +0600

    edit bio

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import Bio from './Bio';
 import './Intro.css';
-const Intro = ({details}) => {
+const Intro = ({details,visitor}) => {
     const initial = {
         bio: details?.bio ? details.bio : "",
         othername: details?.othername ? details.othername : "",
@@ -11,12 +12,39 @@ const Intro = ({details}) => {
         currentCity: details?.currentCity ? details.currentCity : "Tanger",
         hometown: details?.hometown ? details.hometown : "Morocco",
         relationship: details?.relationship ? details.relationship : "Single",
-        instagram: details?.instagram ? details.instagram : "med_hajji7",
+        instagram: details?.instagram ? details.instagram : "saymanrabbi",
       };
       const [infos, setInfos] = useState(initial);
+      const [showBio, setShowBio] = useState(true);
+      const [max, setMax] = useState(infos?.bio ? 100 - infos?.bio.length : 100);
+      const handleBioChange = (e) => {
+        setInfos({ ...infos, bio: e.target.value });
+        setMax(100 - e.target.value.length);
+      };
       return (
         <div className="profile_card">
           <div className="profile_card_header">Intro</div>
+          {infos?.bio && !showBio && (
+        <div className="info_col">
+          <span className="info_text">{infos.bio}</span>
+          {!visitor && (
+            <button
+              className="gray_btn hover1"
+              onClick={() => setShowBio(true)}
+            >
+              Edit Bio
+            </button>
+          )}
+        </div>
+      )}
+      {showBio && (
+        <Bio
+          infos={infos}
+          max={max}
+          handleBioChange={handleBioChange}
+          setShowBio={setShowBio}
+        />
+      )}
           {infos.job && infos.workplace ? (
             <div className="info_profile">
               <img src="../../../icons/job.png" alt="" />
@@ -77,6 +105,15 @@ const Intro = ({details}) => {
               </a>
             </div>
           )}
+          {!visitor && (
+        <button className="gray_btn hover1 w100">Edit Details</button>
+      )}
+      {!visitor && (
+        <button className="gray_btn hover1 w100">Add Hobbies</button>
+      )}
+      {!visitor && (
+        <button className="gray_btn hover1 w100">Add Featured</button>
+      )}
         </div>
       );
 };
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -85,7 +85,7 @@ export default function Profile({setVisible}) {
             <PplYouMayKnow />
             <div className="profile_grid">
               <div className="profile_left">
-                <Intro details={profile.details}/>
+                <Intro details={profile.details} visitor={visitor}/>
                 <Photos photos={photos}/>
                 <Friends friends={profile.friends}/>
                 <div className="relative_fb_copyright">
