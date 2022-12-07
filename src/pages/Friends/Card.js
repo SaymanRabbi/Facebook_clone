7d9b8b10bd08e540e12a00be01c40c9e35ce312c
saymanrabbi <mdrabbi.asm@gmail.com>
2022-12-07 19:47:21 +0600

    friend page

--- src/pages/Friends/Card.js
+++ src/pages/Friends/Card.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
-const Card = () => {
+import { Link } from "react-router-dom";
+const Card = ({ user, type }) => {
     return (
-        <div>
-            
-        </div>
+        <div className="req_card">
+      <Link to={`/profile/${user.username}`}>
+        <img src={user.picture} alt="" />
+      </Link>
+      <div className="req_name">
+        {user.first_name} {user.last_name}
+      </div>
+      {type === "sent" ? (
+        <button className="blue_btn">Cancel Request</button>
+      ) : type === "request" ? (
+        <>
+          <button className="blue_btn">Confirm</button>
+          <button className="gray_btn">Delete</button>
+        </>
+      ) : (
+        ""
+      )}
+    </div>
     );
 };
 
--- src/pages/Friends/Friends.js
+++ src/pages/Friends/Friends.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import Header from '../../components/Header/Header';
 import { friendspage } from '../../func/reducers';
 import { getFriend } from '../../func/user';
+import Card from './Card';
 import './Friends.css';
 const Friends = () => {
   const {user} = useSelector((state)=>({...state}))
@@ -18,12 +19,15 @@ const Friends = () => {
 const getdata = async () => {
   dispatch({ type: "FRIENDS_REQUEST" });
     const data = await getFriend(user.token);
-    if (data.status === "ok") {
+    console.log(data)
+    if (data.status === 200) {
       dispatch({ type: "FRIENDS_SUCCESS", payload: data.data });
+      // console.log(data.data)
     } else {
       dispatch({ type: "FRIENDS_ERROR", payload: data.data });
     }
 }
+console.log(data)
     return (
         <>
         <Header page="friends" />
@@ -98,7 +102,44 @@ const getdata = async () => {
               </div>
             </div>
           </div>
-          <div className="friends_right"></div>
+          <div className="friends_right">
+          <div className="friends_right_wrap">
+            <div className="friends_left_header">
+              <h3>Friend Requests</h3>
+              <a className="see_link hover3">See all</a>
+            </div>
+            <div className="flex_wrap">
+              {data.requests &&
+                data.requests.map((user) => (
+                  <Card user={user} key={user._id} type="request" />
+                ))}
+            </div>
+          </div>
+          <div className="friends_right_wrap">
+            <div className="friends_left_header">
+              <h3>Sent Requests</h3>
+              <a className="see_link hover3">See all</a>
+            </div>
+            <div className="flex_wrap">
+              {data.sentRequests &&
+                data.sentRequests.map((user) => (
+                  <Card user={user} key={user._id} type="sent" />
+                ))}
+            </div>
+          </div>
+          <div className="friends_right_wrap">
+            <div className="friends_left_header">
+              <h3>Friends</h3>
+              <a className="see_link hover3">See all</a>
+            </div>
+            <div className="flex_wrap">
+              {data.friends &&
+                data.friends.map((user) => (
+                  <Card user={user} key={user._id} type="friends" />
+                ))}
+            </div>
+          </div>
+        </div>
         </div>
       </>
     );
