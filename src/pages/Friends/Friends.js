2dca0930eccc726798bbc8dcbd838100789f5aa3
saymanrabbi <mdrabbi.asm@gmail.com>
2022-12-07 18:11:53 +0600

    Friends page

---
+++ src/pages/Friends/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Friends.css';
+const Friends = () => {
+    return (
+        <div>
+            Friends
+        </div>
+    );
+};
+
+export default Friends;
